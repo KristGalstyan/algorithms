e55f8acbbff67639bd550b0fda43af0139719164
Krist <kitog@bk.ru>
2022-12-16 14:47:54 +0400

    tree algorithm on iterator

--- 8_tree_algs.js
+++ 8_tree_algs.js
@@ -43,7 +43,6 @@ const recursive = (tree) => {
     let sum = 0;
     tree.forEach(item => {
         sum += item.v;
-        console.log(sum);
         if (!item.c) {
             return item.v;
         }
@@ -52,4 +51,23 @@ const recursive = (tree) => {
     return sum;
 };
 
-console.log(recursive(tree));
+
+const iteration = (tree) => {
+    if (!tree.length) {
+        return 0;
+    }
+    let sum = 0;
+    let stack = [];
+  tree.forEach(node => stack.push(node));
+  while (stack.length) {
+    const node = stack.pop();
+    sum += node.v;
+    if (node.c) {
+        node.c.forEach(child => stack.push(child));
+    }
+  }
+  return sum;
+};
+
+console.log(iteration(tree));
+// console.log(recursive(tree));
